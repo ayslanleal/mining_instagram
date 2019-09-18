80bb56e85baa6ea32d38909910391fe17982cc5c
ayslanl <ayslanleal15@gmail.com>
2019-09-17 21:25:03 -0300

    adicionando scrollto

--- teste.py
+++ teste.py
@@ -10,18 +10,18 @@ binary = FirefoxBinary('C:\\Program Files\\Mozilla Firefox\\firefox.exe')
 driver = webdriver.Firefox(firefox_binary=binary, executable_path = r'C:\\geckodriver.exe')
 driver.get("https://www.instagram.com/"+username)
 
+#Pagelength = driver.execute_script("window.scrollTo(document.body.scrollHeight/1.5, document.body.scrollHeight/25.0);")
+Comp_pag = driver.execute_script("window.scrollTo(0, document.body.scrollHeight/1.5);")
+
 links = []
+Comp_pag = driver.execute_script("window.scrollTo(document.body.scrollHeight/1.5, document.body.scrollHeight/3);")
 pesquisa = driver.page_source
 data =bs(pesquisa, 'html.parser')
 body = data.find('body')
 script = body.find('span')
 for link in script.findAll('a'):
-    if re.match("/p", link.get('href')):
-        links.append('https://www.instagram.com/'+link.get('href'))
+        if re.match("/p", link.get('href')):
+                links.append('https://www.instagram.com/'+link.get('href'))
 
-time.sleep(3)
+time.sleep(5)
 print(links)
-
-
-
-
